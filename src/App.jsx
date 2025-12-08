d3f99b7e432ab98f0ba02712280e531b1756a0fb
IliaTikyov <iliatikiov123@gmail.com>
2025-12-08 15:20:46 +0200

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Layout from "./components/Layout";
+import Home from "./pages/Home";
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Layout>
+    </BrowserRouter>
+  );
+};
+
+export default App;
